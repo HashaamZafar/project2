c9ec552d68aabca6ad9ddd26683b7a22fa3ca8e4
Noob Scorpio <aliadam7867@gmail.com>
2021-01-11 22:26:12 +0500

    NEW COMMIT

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const mongo = require ('./db'); 
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient; 
 // const {insertUser}=require ('./db'); 
 const express = require('express')
 const expressLayouts = require('express-ejs-layouts')
 const bodyParser= require('body-parser');
 const app = express()
 const port = 5000
-
-
-
+var url ="mongodb://localhost:27017/mydb"
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+  });
 // Static Files
 app.use(express.static("views"))
 // Example for other folders - not required
@@ -40,7 +44,19 @@ app.get('/contact', (req, res) => {
 app.post('/home', async(req, res) => {
     var Name = req.body.name;
     var Gender= req.body.gender;
-    await mongo.insertUser(Name,Gender);
+    try{
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("mydb");
+            var myobj = { name: Name, gender: Gender };
+            dbo.collection("users").insertOne(myobj, function(err, res) {
+              if (err) throw err;
+              console.log("1 document inserted");
+              db.close();
+            });
+          });
+    }catch(e){console.log(e)}
+    
     console.log(req.body.name,req.body.gender);
     res.render('user', {data: req.body});
 })
@@ -49,7 +65,23 @@ app.get('/home', (req, res) => {
     res.render('home')
 })
 app.get('/user', (req, res) => {
-    res.render('user', { title: 'Users'})
+    var data;
+    try{
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("mydb");
+            dbo.collection("users").find({}).toArray(function(err, result) {
+              if (err) throw err;
+              data=result
+              console.log(data[0]['name']);
+              db.close();
+            });
+          });
+    }
+    catch(e){
+        console.log(e)
+    }
+    // res.render('user', { title: 'Users'})
 })
 
 // Listen on Port 5000
--- db.js
+++ db.js
@@ -1,33 +1,33 @@
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/mydb";
+// var MongoClient = require('mongodb').MongoClient();
+// var url = "mongodb://localhost:27017/mydb";
 
-MongoClient.connect(url, function(err, db) {
-  if (err) throw err;
-  console.log("Database created!");
-  db.close();
-});
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("mydb");
-    dbo.createCollection("users", function(err, res) {
-      if (err) throw err;
-      console.log("Collection created!");
-      db.close();
-    });
-  });
+// MongoClient.connect(url, function(err, db) {
+//   if (err) throw err;
+//   console.log("Database created!");
+//   db.close();
+// });
+// // MongoClient.connect(url, function(err, db) {
+// //     if (err) throw err;
+// //     var dbo = db.db("mydb");
+// //     dbo.createCollection("users", function(err, res) {
+// //       if (err) throw err;
+// //       console.log("Collection created!");
+// //       db.close();
+// //     });
+// //   });
 
-module.exports=function(){
-    this.insertUser=function insertUser(uname,ugender){
-        MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db("mydb");
-            var myobj = { name: uname, gender: ugender };
-            dbo.collection("users").insertOne(myobj, function(err, res) {
-              if (err) throw err;
-              console.log("1 document inserted");
-              db.close();
-            });
-          });
+// module.exports=function(){
+//     this.insertUser=function insertUser(uname,ugender){
+//         MongoClient.connect(url, function(err, db) {
+//             if (err) throw err;
+//             var dbo = db.db("mydb");
+//             var myobj = { name: uname, gender: ugender };
+//             dbo.collection("users").insertOne(myobj, function(err, res) {
+//               if (err) throw err;
+//               console.log("1 document inserted");
+//               db.close();
+//             });
+//           });
           
-    };
-};
+//     };
+// };
